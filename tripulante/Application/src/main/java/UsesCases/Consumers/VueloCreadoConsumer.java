26bcccfecd82b72107ac0457812716c8d7a9c2a0
Licethd <liceth.dfe@gmail.com>
2022-09-20 02:11:29 -0400

    changes

---
+++ tripulante/Application/src/main/java/UsesCases/Consumers/VueloCreadoConsumer.java
@@ -0,0 +1,19 @@
+package UsesCases.Consumers;
+
+import core.IntegrationEvent;
+import Fourteam.massTransit.IConsumer;
+import Fourteam.mediator.IMediator;
+
+public class VueloCreadoConsumer extends IConsumer<IntegrationEvents.VueloCreado> {
+
+  public VueloCreadoConsumer(IMediator mediator) {
+    System.out.println("Entro al constructor del consumer");
+  }
+
+  @Override
+  public void Consume(IntegrationEvents.VueloCreado object) {
+    // TODO Auto-generated method stub
+    System.out.println("Entro al consumido " + object.keyTripulacion);
+
+  }
+}
--- tripulante/Application/src/main/java/UsesCases/DomainEventHandler/Tripulacion/PublishIntegrationEventWhenTripulacionChangeHandler.java
+++ tripulante/Application/src/main/java/UsesCases/DomainEventHandler/Tripulacion/PublishIntegrationEventWhenTripulacionChangeHandler.java
@@ -13,40 +13,42 @@ import Model.Tripulacion.Tripulacion;
 import Repositories.ITripulacionRepository;
 
 public class PublishIntegrationEventWhenTripulacionChangeHandler
-  implements NotificationHandler<ConfirmedDomainEvent<TripulacionChange>> {
+		implements NotificationHandler<ConfirmedDomainEvent<TripulacionChange>> {
 
-  private IPublishEndpoint publishEndpoint;
-  private ITripulacionRepository _tripulacionRepository;
+	private IPublishEndpoint publishEndpoint;
+	private ITripulacionRepository _tripulacionRepository;
 
-  public PublishIntegrationEventWhenTripulacionChangeHandler(IPublishEndpoint publishEndpoint,ITripulacionRepository tripulacionRepository) {
-    this.publishEndpoint = publishEndpoint;
-	this._tripulacionRepository = tripulacionRepository;
-  }
+	public PublishIntegrationEventWhenTripulacionChangeHandler(IPublishEndpoint publishEndpoint,
+			ITripulacionRepository tripulacionRepository) {
+		this.publishEndpoint = publishEndpoint;
+		this._tripulacionRepository = tripulacionRepository;
+	}
 
-  @Override
-  public void handle(Notification notification) {
-    ConfirmedDomainEvent event = (ConfirmedDomainEvent) notification;
-	try {
-		TripulacionChange tripulacionChange = (TripulacionChange) event.DomainEvent;
-		Tripulacion tripulacion = _tripulacionRepository.FindByKey(tripulacionChange.KeyTripulacion);
-		IntegrationEvents.TripulacionChange evento = new IntegrationEvents.TripulacionChange();
-		evento.Key = tripulacion.key;
-		evento.descripcion = tripulacion.getDescripcion();
-		List<IntegrationEvents.dto.TripulanteDto> arrTripulantes = new ArrayList<>();
-		tripulacion.Tripulantes.iterator().forEachRemaining(tripulante -> {
-		  IntegrationEvents.dto.TripulanteDto tripulanteDto = new IntegrationEvents.dto.TripulanteDto();
-		  tripulanteDto.key = tripulante.key;
-		  tripulanteDto.nombre = tripulante.getNombre();
-		  tripulanteDto.apellido = tripulante.getApellido();
-		  tripulanteDto.tipo = tripulante.getTipo();
-		  arrTripulantes.add(tripulanteDto);
-		});
-		evento.tripulantes = arrTripulantes;
-		this.publishEndpoint.Publish(evento);
-	  } catch (Exception e) {
-		e.printStackTrace();
-	  }
+	@Override
+	public void handle(Notification notification) {
+		ConfirmedDomainEvent event = (ConfirmedDomainEvent) notification;
+		try {
+			TripulacionChange tripulacionChange = (TripulacionChange) event.DomainEvent;
+			Tripulacion tripulacion = _tripulacionRepository.FindByKey(tripulacionChange.KeyTripulacion);
+			IntegrationEvents.TripulacionChange evento = new IntegrationEvents.TripulacionChange();
+			evento.Key = tripulacion.key;
+			evento.descripcion = tripulacion.getDescripcion();
+			List<IntegrationEvents.dto.TripulanteDto> arrTripulantes = new ArrayList<>();
+			tripulacion.Tripulantes.iterator().forEachRemaining(tripulante -> {
+				IntegrationEvents.dto.TripulanteDto tripulanteDto = new IntegrationEvents.dto.TripulanteDto();
+				tripulanteDto.key = tripulante.key;
+				tripulanteDto.nombre = tripulante.getNombre();
+				tripulanteDto.apellido = tripulante.getApellido();
+				tripulanteDto.tipo = tripulante.getTipo();
 
+				tripulanteDto.setCargo(tripulante.getCargo().getDescripcion());
+				arrTripulantes.add(tripulanteDto);
+			});
+			evento.tripulantes = arrTripulantes;
+			this.publishEndpoint.Publish(evento);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-  }
+	}
 }
